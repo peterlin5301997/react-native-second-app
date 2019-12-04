4ab5b4752422a931ea543a42ee286b164f779517
Flatiron Access Labs <flatironaccesslabs@Flatirons-MacBook-Air.local>
2019-12-04 14:05:27 -0500

    adds StartGameScreen component

---
+++ components/StartGameScreen.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+
+const StartGameScreen = props => {
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>Start a New Game!</Text>
+      <View style={styles.inputContainer}>
+        <Text>Select a Number</Text>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <Button title='Reset' />
+          <Button title='Confirm' />
+        </View>
+      </View>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: '80%',
+    alignItems: 'center',
+    // Shadow iOS only
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    // elevation android only
+    elevation: 8,
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15
+  }
+});
+
+export default StartGameScreen;
