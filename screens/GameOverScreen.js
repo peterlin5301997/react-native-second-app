695963d0bd1f1b5911f3f79f11275d3d09d20655
Flatiron Access Labs <flatironaccesslabs@Flatirons-MacBook-Air.local>
2019-12-06 15:42:02 -0500

    adds styling to GameOverScreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
+import Card from '../components/Card';
+import Colors from '../constants/colors';
+
 const GameOverScreen = props => {
   return (
     <View style={styles.screen}>
-      <Text>The Game is Over!</Text>
-      <Text>Number of Rounds: {props.rounds}</Text>
-      <Text>Number was {props.userNumber}</Text>
-      <Button title='NEW GAME' onPress={props.onRestart} />
+      <Card>
+        <Text style={styles.title}>The Game is Over!</Text>
+        <Text style={styles.details}>Number of Rounds: {props.rounds}</Text>
+        <Text style={styles.details}>Number was: {props.userNumber}</Text>
+        <View style={styles.buttonContainer}>
+          <Button title='NEW GAME' onPress={props.onRestart} />
+        </View>
+      </Card>
     </View>
   );
 }
@@ -17,6 +24,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  buttonContainer: {
+    marginTop: 20
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginVertical: 10,
+    color: Colors.accent
+  },
+  details: {
+    fontSize: 15,
+    marginVertical: 5
   }
 })
 
